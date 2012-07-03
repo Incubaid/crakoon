6b389203f167751e9caa92cc79004e3ef9c2e384
Bastiaan Stougie <bastiaan.stougie@amplidata.com>
2012-07-03 15:36:26 +0200

    doxygen documentation for the Arakoon C++ API
    See:  ARAKOON-303
    See: http://jira.incubaid.com/browse/ARAKOON-303

--- src/arakoonmm.hpp
+++ src/arakoonmm.hpp
@@ -33,27 +33,122 @@
 #include <string>
 #include <exception>
 
+/**
+ * \namespace arakoon
+ *
+ * \par Requirements
+ * The Crakoon C++ API uses the C++11 standard, which may require specific
+ * compiler options, for example: 'g++ -std=c++0x'.
+ * The C++ API is a wrapper around the Crakoon C API, so both the Crakoon C API
+ * library and the Crakoon C++ API library should be linked to.
+ *
+ * \par Buffers
+ * Arakoon can store keys and values containing NUL bytes. The C implementation
+ * underlying the C++ implementation does its own memory allocations. Currently,
+ * in C++, there is no way to may a std::string of a blob of data without
+ * copying the data, and copying can be expensive. To avoid the copy, a 'buffer'
+ * class is provided instead.
+ *
+ * \par Memory handling
+ * The memory handling procedures to be used by Crakoon can be set through
+ * ::arakoon::memory_set_hooks.
+ * All memory management has been simplified greatly by use of std::shared_ptr.
+ * All returned objects will take care of deallocating themselves properly when
+ * going out of scope.
+ *
+ * \par Error handling
+ * Arakoon errors are translated to one of the specific ::arakoon::error_*
+ * exceptions.
+ * Memory allocation errors are translated to std::bad_alloc; any other
+ * errno code from the underlying Crakoon C API is translated to
+ * a std::runtime_error with strerror(errno) as what().
+ */
 namespace arakoon {
 
+/** \defgroup arakoon_cpp_api_group Arakoon C++ API
+ * @{
+ */
+
+/**
+ * \brief
+ * Set the memory handling procedures to be used by Crakoon. The `malloc`,
+ * `free` and `realloc` functions can be specified. These functions should *NOT*
+ * throw any exceptions, since they are called from C code. Crakoon should not
+ * use any other heap-allocation functions.
+ *
+ * The system *malloc(3)*, *free(3)* and *realloc(3)* procedures are used as default.
+ * If possible, it is advisable to register your own heap allocation handler
+ * functions before calling any other Crakoon procedures.
+ *
+ * For the definition of #ArakoonMemoryHooks, please see arakoon.h.
+ */
 void memory_set_hooks(ArakoonMemoryHooks const * const hooks);
 
-//// buffer
-
+/** \defgroup buffer_group Buffers
+ * @{
+ */
+/**
+ * \class buffer
+ *
+ * Arakoon can store keys and values containing NUL bytes. The C implementation
+ * underlying the C++ implementation does its own memory allocations. Currently,
+ * in C++, there is no way to may a std::string of a blob of data without
+ * copying the data, and copying can be expensive. To avoid the copy, a 'buffer'
+ * implementation is provided instead.
+ *
+ * For ease of use, class buffer provides a constructor to wrap a std::string,
+ * without copying the data, so the lifetime of the string should exceed
+ * the lifetime of the buffer.
+ *
+ * A buffer can be told to take ownership of the raw data provided to its
+ * constructor, in which case it will free it on destruction using the
+ * deallocation function in use (see 'Memory handling' below).
+ */
 class buffer
 {
   public:
+    /**
+     * \brief Construct a buffer that represents the value 'None', with
+     *        data == NULL and size == 0.
+     */
     buffer();
 
+    /**
+     * \brief Construct a buffer from a string's data and size. The ownership
+     *        of the data remains with the string, so the caller should take
+     *        care that the lifetime of the string exceeds the lifetime of the
+     *        buffer.
+     */
     buffer(std::string const & s);
 
+    /**
+     * \brief Construct a buffer from specified data and size. If told to take
+     *        ownership of the data, the buffer will deallocate the data on
+     *        reset, set, or destruction using the deallocation function in use
+     *        (see ::arakoon::memory_set_hooks).
+     */
     buffer(
         void * const data,
         std::size_t const size,
         bool const take_ownership = false);
 
+    /**
+     * \brief If the buffer currently has ownership of its data, the destructor
+     *        deallocates it using the deallocation function in use (see
+     *        ::arakoon::memory_set_hooks).
+     */
     ~buffer();
 
+    /**
+     * \brief If the buffer currently has ownership of its data, deallocate it,
+     *        and refer to 0 bytes of data instead.
+     */
     void reset();
+
+    /**
+     * \brief If the buffer currently has ownership of its data, deallocate it,
+     *        and configure it with the specified parameters as if newly constructed.
+     */
     void set(void * const data, std::size_t const size, bool const take_ownership);
 
     void * data();
@@ -72,19 +167,53 @@ class buffer
 
 typedef std::shared_ptr<buffer> buffer_ptr;
 typedef std::shared_ptr<buffer const> buffer_const_ptr;
+/** @} */ // buffer_group
 
-//// error
-
+/** \defgroup error_group Errors
+ * @{
+ */
 typedef arakoon_rc rc;
 
+/**
+ * \class error
+ *
+ * Class 'error' is the base class for all arakoon errors. It has been derived
+ * from std::exception.
+ *
+ * All specific exception classes ::arakoon::error_* for the different arakoon
+ * #ArakoonReturnCode enumeration values from crakoon derive from 'error'.
+ *
+ * The #ArakoonReturnCode corresponding with the exception can be retrieved
+ * from the ::arakoon::error* exception through method #rc_get(), and the
+ * corresponding error string can be retrieved through method #what().
+ *
+ * Any data the arakoon server returned with the #ArakoonReturnCode can be
+ * retrieved through method buffer_ptr_get().
+ */
 class error : public std::exception
 {
   public:
     error();
     explicit error(buffer_ptr);
     virtual ~error() throw ();
+
+    /**
+     * \brief Return the #ArakoonReturnCode corresponding to the exception.
+     */
     virtual rc rc_get() const = 0;
+
+    /**
+     * \brief Return a buffer with any data the arakoon server returned
+     *        together with the #ArakoonReturnCode. This data may contain
+     *        keys and/or values, which can contain NUL bytes.
+     */
     virtual buffer_const_ptr buffer_ptr_get() const;
+
+    /**
+     * \brief Return the error string corresponding to the #ArakoonReturnCode
+     *        corresponding to the exception, see also arakoon_strerror() in
+     *        arakoon.h.
+     */
     char const * what() const throw();
 
   protected:
@@ -122,7 +251,23 @@ DEF_ERROR(error_client_nursery_invalid_routing, ARAKOON_RC_CLIENT_NURSERY_INVALI
 DEF_ERROR(error_client_nursery_invalid_config, ARAKOON_RC_CLIENT_NURSERY_INVALID_CONFIG);
 
 #undef DEF_ERROR
+/** @} */ // error_group
 
+/** \defgroup data_structures_group Data structures
+ *
+ * \brief Some generic data structures used throughout the API.
+ *
+ * @{
+ */
+
+/** \defgroup value_list_group Lists of keys or values
+ * @{
+ */
+/**
+ * \class value_list_iterator
+ *
+ * Iterator for iteration over a value_list.
+ */
 class value_list_iterator
 {
   public:
@@ -130,9 +275,19 @@ class value_list_iterator
 
     ~value_list_iterator();
 
+    /** \brief Reset the iterator to the first value. */
     void reset();
+
+    /** \brief Advance the iterator to the next value. */
     void next();
+
+    /** \brief Check if the iterator has reached the end of the list. */
     bool at_end() const;
+
+    /** \brief Get the value the iterator is currently positioned at.
+     *         If at the end, the returned buffer has size 0 and data NULL (if
+     *         not at the end, the returned buffer has size 0 but data != NULL).
+     */
     buffer const & value() const;
 
   private:
@@ -145,21 +300,32 @@ class value_list_iterator
 
 typedef std::shared_ptr<value_list_iterator> value_list_iterator_ptr;
 
+/**
+ * \class value_list
+ *
+ * A list of arakoon values, also usable as a list of arakoon keys.
+ */
 class value_list
 {
   public:
+    /** \brief Construct an empty value list. */
     value_list();
 
     value_list(ArakoonValueList * const list);
 
     ~value_list();
 
+    /** \brief Add a copy of the specified value to the list. */
     void add(buffer const & value);
 
+    /** \brief Return an iterator positioned at the first element of the list,
+     *         or if the list is empty at the end of the list.
+     */
     value_list_iterator_ptr begin() const;
 
     ArakoonValueList const * get() const;
 
+    /** \brief Return the number of items in the value_list. */
     size_t size() const;
 
   private:
@@ -170,7 +336,16 @@ class value_list
 };
 
 typedef std::shared_ptr<value_list const> value_list_const_ptr;
+/** @} */ // value_list_group
 
+/** \defgroup key_value_list_group Lists of (key, value) pairs
+ * @{
+ */
+/**
+ * \class key_value_list_iterator
+ *
+ * Iterator for iteration over a key_value_list.
+ */
 class key_value_list_iterator
 {
   public:
@@ -178,10 +353,25 @@ class key_value_list_iterator
 
     ~key_value_list_iterator();
 
+    /** \brief Reset the iterator to the first (key, value) pair. */
     void reset();
+
+    /** \brief Advance the iterator to the next (key, value) pair. */
     void next();
+
+    /** \brief Check if the iterator has reached the end of the list. */
     bool at_end() const;
+
+    /** \brief Get the key the iterator is currently positioned at.
+     *         If at the end, the returned buffer has size 0 and data NULL (if
+     *         not at the end, the returned buffer has size 0 but data != NULL).
+     */
     buffer const & key() const;
+
+    /** \brief Get the value the iterator is currently positioned at.
+     *         If at the end, the returned buffer has size 0 and data NULL (if
+     *         not at the end, the returned buffer has size 0 but data != NULL).
+     */
     buffer const & value() const;
 
   private:
@@ -195,6 +385,11 @@ class key_value_list_iterator
 
 typedef std::shared_ptr<key_value_list_iterator> key_value_list_iterator_ptr;
 
+/**
+ * \class key_value_list
+ *
+ * A list of (key, value) pairs.
+ */
 class key_value_list
 {
   public:
@@ -202,10 +397,14 @@ class key_value_list
 
     ~key_value_list();
 
+    /** \brief Return an iterator positioned at the first element of the list,
+     *         or if the list is empty at the end of the list.
+     */
     key_value_list_iterator_ptr begin() const;
 
     ArakoonKeyValueList const * get() const;
 
+    /** \brief Return the number of items in the value_list. */
     size_t size() const;
 
   private:
@@ -216,21 +415,44 @@ class key_value_list
 };
 
 typedef std::shared_ptr<key_value_list const> key_value_list_const_ptr;
+/** @} */ // key_value_list_group
+/** @} */ // data_structures_group
 
+/** \defgroup sequence_group Sequences
+ * @{
+ */
+/**
+ * \class sequence
+ *
+ * A sequence of arakoon commands.
+ */
 class sequence
 {
   public:
+    /** \brief Construct an empty sequence. */
     sequence();
 
     ~sequence();
 
+    /**
+     * \brief Add a 'set' action to the sequence. Key and value will be
+     *        copied and released on destruction of the sequence.
+     */
     void add_set(
         buffer const & key,
         buffer const & value);
 
+    /**
+     * \brief Add a 'delete' action to the sequence. Key will be
+     *        copied and released on destruction of the sequence.
+     */
     void add_delete(
         buffer const & key);
 
+    /**
+     * \brief Add an 'assert' action to the sequence. Key and value will be
+     *        copied and released on destruction of the sequence.
+     */
     void add_assert(
         buffer const & key,
         buffer const & value);
@@ -245,10 +467,24 @@ class sequence
 };
 
 typedef std::shared_ptr<sequence> sequence_ptr;
+/** @} */ // sequence_group
 
+/** \defgroup client_call_options_group Client call options
+ * @{
+ */
+/**
+ * \class client_call_options
+ *
+ * General options for any call to the arakoon server.
+ */
 class client_call_options
 {
   public:
+    /**
+     * \brief Construct default client call options. See also
+     *        ARAKOON_CLIENT_CALL_OPTIONS_DEFAULT_ALLOW_DIRTY and
+     *        ARAKOON_CLIENT_CALL_OPTIONS_DEFAULT_TIMEOUT in arakoon.h.
+     */
     client_call_options();
 
     ~client_call_options();
@@ -267,14 +503,30 @@ class client_call_options
 
     ArakoonClientCallOptions * options_;
 };
+/** @} */ // client_call_options_group
 
+/** \defgroup cluster_node_group Cluster nodes
+ * @{
+ */
+/**
+ * \class cluster_node
+ *
+ * Representation of an arakoon cluster node.
+ */
 class cluster_node
 {
   public:
+    /** \brief Construct a cluster node with the specified name.
+     *         The name will be copied.
+     */
     cluster_node(std::string const & name);
 
     ~cluster_node();
 
+    /* \brief Add a TCP address to use to connect to the node.
+     *        See the manual page of getaddrinfo(3) for the value of
+     *        'host' and 'service'.
+     */
     void add_address_tcp(
         std::string const & host,
         std::string const & service);
@@ -290,53 +542,115 @@ class cluster_node
 
     ArakoonClusterNode * node_;
 };
+/** @} */ // cluster_node_group
 
+/** \defgroup cluster_group Clusters
+ * @{
+ */
+/**
+ * \class cluster
+ *
+ * Representation of an arakoon cluster node.
+ */
 class cluster
 {
   public:
+    /** \brief Construct a cluster with the specified cluster name.
+     *         The cluster name will be copied.
+     */
     cluster(ArakoonProtocolVersion version, std::string const & cluster_name);
 
     ~cluster();
 
+    /**
+     * \brief Attach a node to the cluster. The cluster assumes ownership of the
+     *        cluster node's internals. Afterward, the cluster node should be
+     *        destructed by the caller.
+     */
     void add_node(cluster_node & node);
 
+    /** \brief Return the cluster name. */
     std::string get_cluster_name() const;
 
+    /**
+     * \brief Look up the master node of the arakoon cluster and connect to it.
+     * \param options Options, or NULL for default options.
+     */
     void connect_master(
         client_call_options const * const options);
 
+    /**
+     * \brief Send a 'hello' call to the server, using 'client_id' and
+     *        'cluster_id', and return the response.
+     * \param options Options, or NULL for default options.
+     */
     std::shared_ptr<std::string> hello(
         client_call_options const * const options,
         std::string const & client_id,
         std::string const & cluster_id);
 
+    /**
+     * \brief Send a 'who_master' call to the server, and return the name of
+     *        the node that is currently the master node of the cluster.
+     * \param options Options, or NULL for default options.
+     */
     std::shared_ptr<std::string> who_master(
         client_call_options const * const options);
 
+    /**
+     * \brief Send an 'expect_progress_possible' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     bool expect_progress_possible(
         client_call_options const * const options);
 
+    /**
+     * \brief Send an 'exists' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     bool exists(
         client_call_options const * const options,
         buffer const & key);
 
+    /**
+     * \brief Send a 'get' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     buffer_ptr get(
         client_call_options const * const options,
         buffer const & key);
 
+    /**
+     * \brief Send a 'multi-get' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     value_list_const_ptr multi_get(
         client_call_options const * const options,
         value_list const & keys);
 
+    /**
+     * \brief Send a 'set' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     void set(
         client_call_options const * const options,
         buffer const & key,
         buffer const & value);
 
+    /**
+     * \brief Send a 'delete' call to the server.
+     * \param options Options, or NULL for default options.
+     */
     void remove(
         client_call_options const * const options,
         buffer const & key);
 
+    /**
+     * \brief Send a 'range' call to the server.
+     *        'begin_key' and 'end_key' can be set to (NULL, 0) to denote 'None'.
+     *        If 'max_elements' is negative, all matches will be returned.
+     * \param options Options, or NULL for default options.
+     */
     value_list_const_ptr range(
         client_call_options const * const options,
         buffer const & begin_key,
@@ -345,6 +659,12 @@ class cluster
         bool const end_key_included,
         ssize_t const max_elements);
 
+    /**
+     * \brief Send a 'range_entries' call to the server.
+     *        'begin_key' and 'end_key' can be set to (NULL, 0) to denote 'None'.
+     *        If 'max_elements' is negative, all matches will be returned.
+     * \param options Options, or NULL for default options.
+     */
     key_value_list_const_ptr range_entries(
         client_call_options const * const options,
         buffer const & begin_key,
@@ -353,6 +673,12 @@ class cluster
         bool const end_key_included,
         ssize_t const max_elements);
 
+    /**
+     * \brief Send a 'rev_range_entries' call to the server.
+     *        'begin_key' and 'end_key' can be set to (NULL, 0) to denote 'None'.
+     *        If 'max_elements' is negative, all matches will be returned.
+     * \param options Options, or NULL for default options.
+     */
     key_value_list_const_ptr rev_range_entries(
         client_call_options const * const options,
         buffer const & begin_key,
@@ -361,21 +687,46 @@ class cluster
         bool const end_key_included,
         ssize_t const max_elements);
 
+    /**
+     * \brief Send a 'prefix' call to the server
+     *        If 'max_elements' is negative, all matches will be returned.
+     * \param options Options, or NULL for default options.
+     */
     value_list_const_ptr prefix(
         client_call_options const * const options,
         buffer const & begin_key,
         ssize_t const max_elements);
 
+    /**
+     * \brief Send a 'test_and_set' call to the server
+     *        'old_value' and 'new_value' can be (NULL, 0) to denote 'None'.
+     *        A result of (NULL, 0) indicates that the server returned 'None'.
+     * \param options Options, or NULL for default options.
+     * \param key The key.
+     * \param old_value The old value that should be tested.
+     * \param new_value The new value that should be set if the test succeeds.
+     * \return The
+     */
     buffer_ptr test_and_set(
         client_call_options const * const options,
         buffer const & key,
         buffer const & old_value,
         buffer const & new_value);
 
+    /**
+     * \brief Send a 'sequence' call to the server.
+     * \param options Options, or NULL for default options.
+     * \param sequence The command sequence to execute.
+     */
     void sequence(
         client_call_options const * const options,
         arakoon::sequence const & sequence);
 
+    /**
+     * \brief Send a 'synced_sequence' call to the server.
+     * \param options Options, or NULL for default options.
+     * \param sequence The command sequence to execute.
+     */
     void synced_sequence(
         client_call_options const * const options,
         arakoon::sequence const & sequence);
@@ -388,6 +739,9 @@ class cluster
 
     ArakoonCluster * cluster_;
 };
+/** @} */ // cluster_group
+
+/** @} */ // arakoon_cpp_api_group
 
 } // namespace arakoon
 
